936174eb7c44a548a9a3c1eb7e14a4c8dbe83abb
slbsh <crafter.antek@gmail.com>
2024-09-08 17:03:22 +0200

    rm clippy::cargo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(forbidden_lint_groups)]
-#![forbid(clippy::complexity, clippy::suspicious, clippy::correctness, clippy::cargo, clippy::perf, clippy::pedantic, clippy::nursery)] 
+#![forbid(clippy::complexity, clippy::suspicious, clippy::correctness, clippy::perf, clippy::pedantic, clippy::nursery)] 
 #![allow(clippy::style, clippy::restriction, clippy::match_bool, clippy::too_many_lines, clippy::single_match_else, clippy::ignored_unit_patterns, clippy::module_name_repetitions, clippy::needless_for_each, clippy::derive_partial_eq_without_eq, clippy::missing_const_for_fn, clippy::cognitive_complexity, clippy::option_if_let_else, clippy::option_map_unit_fn)]
 
 fn main() {
